725abcd8781bb5b79d663d32303fa4a2e331c3b1
cloudhum <turbulentbees@gmail.com>
2022-05-05 15:47:41 -0700

    Implement operate() function

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-function add (a,b){
+function add(a,b){
     return a + b;
 }
 
-function subtract (a,b) {
+function subtract(a,b) {
     return a - b;
 }
 
-function multiply (a,b) {
+function multiply(a,b) {
     return a * b;
 }
 
-function divide (a,b) {
+function divide(a,b) {
     return a/b; 
 }
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1,2));
+function operate(op, a, b) {
+    console.log(op(a,b));
+}
+
+// console.log(add(1,2));
+// console.log(subtract(1,2));
+// console.log(multiply(1,2));
+// console.log(divide(1,2));
+operate(add, 1, 3);
