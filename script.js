cd49696fb183e8ae64145ba2489e8c248975c1e7
cloudhum <turbulentbees@gmail.com>
2022-05-06 12:21:15 -0700

    Leading zero removed on display screen

--- script.js
+++ script.js
@@ -18,12 +18,28 @@ function operate(op, a, b) {
     console.log(op(a,b));
 }
 
-const operatorButtons = document.querySelectorAll('.op-btn');
-const total = document.querySelector('#total');
-console.log(total);
+const numberButtons = document.querySelectorAll('.num-btn');
+const symbolButtons = document.querySelectorAll('.sym-btn');
+const runningEquation = document.querySelector('#running-equation');
+console.log(runningEquation);
+const result = document.querySelector('#total');
+console.log(result);
+const inputtedNums = [];
 
-operatorButtons.forEach(button => {
+numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        total.textContent = button.textContent;
+        inputtedNums.push(button.textContent);
+        //if it's ONLY zero remove it
+        if (result.textContent == "0") result.textContent = "";
+        result.textContent += button.textContent;
+        console.log(inputtedNums);
     })
 });
+
+//when a symbol button is clicked update running equation display
+// symbolButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         inputtedNums.push(button.textContent);
+//         runningEquation.textContent = inputtedNums.toString();
+//     })
+// })
