374c2ae2cf461da63a6370097978f5afb2e5ebab
cloudhum <turbulentbees@gmail.com>
2022-05-11 17:47:44 -0700

    Rework logic to allow several operations to be stringed together in succession

--- script.js
+++ script.js
@@ -2,72 +2,67 @@ function add(a, b) { return a + b }
 function subtract(a, b) { return a - b }
 function multiply(a, b) { return a * b }
 function divide(a, b) { return a / b }
-function operate(op, a, b) { console.log(op(a, b)) }
+function operate(op, a, b) { 
+    a = +a;
+    b = +b;
+    return op(a, b); 
+}
 
-const runningEquation = document.querySelector('#running-equation');
 const result = document.querySelector('#total');
 const clearButton = document.querySelector('#clear-btn');
 const deleteButton = document.querySelector('#delete-btn');
 const equalsButton = document.querySelector('.eq-btn');
 const numberButtons = document.querySelectorAll('.num-btn');
 const symbolButtons = document.querySelectorAll('.sym-btn');
-const inputtedNums = [];
 
-console.log(clearButton);
-console.log(deleteButton);
-console.log(runningEquation);
-console.log(result);
+let firstValue = 0;
+let secondValue = 0;
+let currentOperator = '';
+let newResult = 0;
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        inputtedNums.push(button.textContent);
-        if (result.textContent == "0") result.textContent = "";
+        if (currentOperator != '') result.textContent = '';
         result.textContent += button.textContent;
-        console.log(inputtedNums);
-    })
-});
-
-//when a symbol button is clicked update runningEquation in display
-symbolButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        inputtedNums.push(button.textContent);
-        runningEquation.textContent = inputtedNums.toString();
-        result.textContent = " "
     })
 });
 
 clearButton.addEventListener('click', () => {
     result.textContent = " ";
-    inputtedNums.length = 0;
-    runningEquation.textContent = " ";
+    firstValue = 0;
+    secondValue = 0;
+    currentOperator = '';
 });
 
-deleteButton.addEventListener('click', () => {
-    result.textContent = " ";
+symbolButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (firstValue == 0 && secondValue == 0){
+            currentOperator = button.textContent;
+            firstValue = result.textContent;
+        }
+        else if (firstValue != 0 && secondValue == 0){
+            secondValue = result.textContent;
+            currentOperator = opSymbolToString(currentOperator);
+            // pull newResult out into global scope
+            // remove plus symbols below
+            newResult = operate(currentOperator, firstValue, secondValue);
+            firstValue = newResult;
+            result.textContent = newResult;
+            currentOperator = button.textContent;
+            secondValue = 0;
+        }
+    })
 });
 
 equalsButton.addEventListener('click', () => {
-    let parsed = formatInput(inputtedNums);
-    console.log("Parsed:", parsed);
-    operate(parsed[0], parsed[1], parsed[2]);
+    // FIXME: pressing = before entering all the numbers or an operator causes problems
+    if(firstValue != 0 && secondValue == 0) secondValue = result.textContent;
+    currentOperator = opSymbolToString(currentOperator);
+    newResult = operate(currentOperator, firstValue, secondValue); 
+    firstValue = newResult;
+    result.textContent = newResult;
 });
 
-function formatInput(arr) {
-    let op;
-
-    arr.forEach(x => {
-        if (x == "+" || x == "-" || x == "/" || x == "*")
-            op = x;
-    });
-
-    arr = arr.join('').split(`${op}`)
-    let firstValue = +(arr[0]);
-    let secondValue = +(arr[1]);
-    op = opSymbolToString(op);
-    return [op, firstValue, secondValue];
-}
-
-// pass symbol: + - / * and receive plus minus divide multiply 
 function opSymbolToString(op) {
     if (op == "+") return add;
     if (op == "-") return subtract;
